72f4866294a56bd624917b92bd8727a485ffb064
k4tzenfle1sh <nekratov2004@yandex.ru>
2021-06-30 12:25:43 +0700

    final release, done
    nice one dude

--- DEMO_EXAM/DEMO_EXAM.cpp
+++ DEMO_EXAM/DEMO_EXAM.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include "variables.h"
 
-int is_min(int a, int b) {
+int is_min(int a, int b) { //функция сравнения двух чисел
 	if (a <= b)
 		return a;
 	else
@@ -14,55 +14,52 @@ int is_min(int a, int b) {
 int main() {
 	setlocale(LC_ALL, "rus");
 	std::cout << "Согласно заданию, необходимо найти кратчайшее расстояние между узлами 1 и 7 на графе, данным в задании.\nНайдем минимальные расстояния:\n" << std::endl;
-	bool is_Done = false;
-	bool is_FirstRun = true;
-	int counter = NULL;
+	
 	while (!is_Done) {
-		if (is_FirstRun) {
-			std::cout << "U[0] = 0," << std::endl;
-			EASY_WAY.push_back(0);
-			is_FirstRun = false;
-			counter++;
+		if (is_FirstRun) { //если первый запуск
+			std::cout << "U[0] = 0," << std::endl; 
+			EASY_WAY.push_back(0); //первая сумма есть 0
+			is_FirstRun = false; //снимаем флаг
+			counter++; //переходим на новый узел
 			continue;
 		}
 		while (1) {
-			int count = 0;
 			for (int i = 0; i < i_WAY_AXIS; i++) {
-				if (TABLE_VECTORS[i].ui_END_AXIS == counter) {
-					WAY.push_back(TABLE_VECTORS[i].ui_LONG_WAY + EASY_WAY[TABLE_VECTORS[i].ui_START_AXIS].V);
+				if (TABLE_VECTORS[i].ui_END_AXIS == counter) { //ищем совпадение по узлам графа и нужного для поиска
+					WAY.push_back(TABLE_VECTORS[i].ui_LONG_WAY + EASY_WAY[TABLE_VECTORS[i].ui_START_AXIS].SUMM); //впихиваем во временный массив возможный переход
 				}
 			}
 			break;
 		}
 		std::cout << "U[" << counter << "] = ";
-		if (WAY.size() >= 2) {
+		if (WAY.size() >= 2) { //если переходов больше двух для одного узла, то
 			int MIN = INT_MAX;
 			for (int i = 0; i < WAY.size(); i++) {
 				if (i == 0) {
-					MIN = WAY[i].summ;
+					MIN = WAY[i].SUMM;
 					continue;
 				}
-				MIN = is_min(MIN, WAY[i].summ);
+				MIN = is_min(MIN, WAY[i].SUMM); //ищем минимальную сумму по ним
 			}
-			EASY_WAY.push_back(MIN);
+			EASY_WAY.push_back(MIN); //вносим запись в путеводитель
 			std::cout << MIN;
 		}
-		else {
-			std::cout << WAY[0].summ;
-			EASY_WAY.push_back(WAY[0].summ);
+		else { //иначе
+			std::cout << WAY[0].SUMM;
+			EASY_WAY.push_back(WAY[0].SUMM); //просто вносим первый эелемент сразу
 		}
 
-		if (counter == FINAL) {
-			is_Done = true;
+		if (counter == FINAL) { //если искомый узел уперся в конечный для поиска
+			is_Done = true; //то программа завершила свою работу
 			std::cout << "." << std::endl;
 		}
-		else {
-			counter++;
+		else { //иначе
+			counter++; //переход на новый узел
 			std::cout << "," << std::endl;
-			WAY.clear();
+			WAY.clear(); //очищаем временный вектор
 		}
 	}
-	std::cout << "\nКратчайшее расстояние между узлами 1 и 7 равняется: " << EASY_WAY[EASY_WAY.size()-1].V << "." << std::endl;
+	std::cout << "\nКратчайшее расстояние между узлами 1 и 7 равняется: " << EASY_WAY[EASY_WAY.size()-1].SUMM << "." << std::endl;
 	system("pause");
 	return 0;
 }
--- DEMO_EXAM/variables.h
+++ DEMO_EXAM/variables.h
@@ -1,37 +1,36 @@
 #ifndef VAR_H
 #define VAR_H
-struct Table {
-	int ui_START_AXIS;
-	int ui_END_AXIS;
-	int ui_LONG_WAY;
-	bool is_Checked = false;
-	/*Way(unsigned int aa, unsigned int bb, unsigned int cc) {
-		ui_START_AXIS = aa;
-		ui_END_AXIS = bb;
-		ui_LONG_WAY = cc;
-	};*/
+struct Table { //структура всех путей
+	int ui_START_AXIS; //начальный узел
+	int ui_END_AXIS; //конечный узел
+	int ui_LONG_WAY; //расстояние
 };
 
-struct Way {
-	int summ = NULL;
-	Way(int cc) {
-		summ = cc;
+struct Way { //временный массив
+	int SUMM = NULL; //общее расстояние
+	Way(int cc) { //конструктор
+		SUMM = cc;
 	};
 };
 
-struct easy {
-	int V = NULL;
-	easy(int cc) {
-		V = cc;
+struct EasyWay {
+	int SUMM = NULL;
+	EasyWay(int a) {
+		SUMM = a;
 	};
 };
 
-std::vector <Table> TABLE_VECTORS = { {0,1,2},{0,2,4}, {0,3,10}, {1,3,11}, {1,4,5}, {2,3,3},{2,5,1}, {3,4,8},{3,5,7},{4,6,6},{5,6,9} };
-std::vector <Way> WAY;
-std::vector <easy> EASY_WAY;
+std::vector <Table> TABLE_VECTORS = { {0,1,2},{0,2,4}, {0,3,10}, {1,3,11}, {1,4,5}, {2,3,3},{2,5,1}, {3,4,8},{3,5,7},{4,6,6},{5,6,9} }; //статическое поле объектов
+std::vector <Way> WAY; //временный вектор
+std::vector <EasyWay> EASY_WAY; //общий вектор пути
 
-const int i_COUNT_AXIS = 7;
-const int i_WAY_AXIS = 11;
+const int i_COUNT_AXIS = 7; //число всех узлов в графе
+const int i_WAY_AXIS = 11; //число всех путей в нем
 
-int FINAL = 6;
+int FINAL = 6; //узел, для которого выполянется поиск
+int counter = NULL; //счетчик перехода на новый узел
+
+bool is_Done = false; //булевая для главного цикла
+bool is_FirstRun = true; //булевая первого запуска
+
 #endif
